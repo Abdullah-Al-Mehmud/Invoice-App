a7c2ef04eff67083953616663734d30ca519fe55
Abdullah Al Mehmud <timehmud121@gmail.com>
2024-07-26 20:32:29 +0600

    added form table and add more forms functionality

--- src/Components/AddInvoice/Invoice.jsx
+++ src/Components/AddInvoice/Invoice.jsx
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import InvoiceForm from "./InvoiceForm";
 import InvoiceInput from "./InvoiceInput";
 import InvoiceTable from "./InvoiceTable";
 
 const Invoice = () => {
+  const [addMoreItems, setAddMoreItems] = useState([
+    {
+      id: 1,
+      item: "",
+      description: "",
+      Qty: "",
+      unitPrice: "",
+      disc: "",
+      account: "",
+      taxRate: "",
+      amount: "",
+    },
+  ]);
+
+  const handleAddMore = () => {
+    setAddMoreItems([
+      ...addMoreItems,
+      {
+        id: addMoreItems.length + 1,
+        item: "",
+        description: "",
+        Qty: "",
+        unitPrice: "",
+        disc: "",
+        account: "",
+        taxRate: "",
+        amount: "",
+      },
+    ]);
+  };
+
   return (
     <div className="w-full ">
       <div className=" lg:pl-44 h-auto">
         <div className="bg-[#e4e4e4] mx-3 h-auto py-5 mt-5">
           <InvoiceForm />
           <InvoiceInput />
-          <InvoiceTable />
+          <InvoiceTable addMoreItems={addMoreItems} />
+          <div className="px-4 mt-4">
+            <button
+              onClick={handleAddMore}
+              className="px-7 py-2 bg-green-500 text-white">
+              Add More
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/AddInvoice/InvoiceTable.jsx
+++ src/Components/AddInvoice/InvoiceTable.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { useState } from "react";
+import { RxDragHandleDots2 } from "react-icons/rx";
 
-const InvoiceTable = () => {
+const InvoiceTable = ({ addMoreItems }) => {
   return (
-    <div className="mx-4 bg-white shadow-md rounded-md overflow-x-auto">
+    <div className="mx-4 bg-white shadow-md rounded-md overflow-x-auto mt-4">
       <table className="min-w-full divide-y divide-gray-200">
         <thead className="bg-[#e8faf7]">
           <tr>
+            <th></th>
             <th
               scope="col"
               className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -50,43 +52,81 @@ const InvoiceTable = () => {
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200 ">
-          <tr className="">
-            <td className="border border-gray-200 py-4 whitespace-nowrap text-sm text-gray-500">
-              <input type="text" className="" />
-            </td>
-            <td className=" border border-gray-200 py-4 whitespace-nowrap text-sm text-gray-900">
-              {" "}
-              <input type="text" className=" " />
-            </td>
-            <td className=" border border-gray-200 w-12 py-4 whitespace-nowrap text-sm text-gray-500">
-              {" "}
-              <input type="text" className=" w-full" />
-            </td>
-            <td className=" border border-gray-200 w-32 py-4 whitespace-nowrap text-sm text-gray-500">
-              {" "}
-              <input type=" text" className="w-full" />
-            </td>
-            <td className=" border border-gray-200 w-24 py-4 whitespace-nowrap text-sm text-gray-500">
-              {" "}
-              <input type="text" className="w-full" />
-            </td>
-            <td className=" border border-gray-200 w-32 py-4 whitespace-nowrap text-sm text-gray-500">
-              {" "}
-              <input type="text" className="w-full" />
-            </td>
-            <td className=" border border-gray-200 py-4 whitespace-nowrap text-sm text-gray-500">
-              {" "}
-              <input type="text" className="w-full" />
-            </td>
-            <td className=" border border-gray-200 py-4 whitespace-nowrap text-sm text-gray-500">
-              {" "}
-              <input type="text" className="w-full" />
-            </td>
-            <td className=" border border-gray-200 py-4 whitespace-nowrap text-sm text-gray-500">
-              {" "}
-              x
-            </td>
-          </tr>
+          {addMoreItems.map((item) => (
+            <tr key={item?.id} className="">
+              <td>
+                <RxDragHandleDots2 />
+              </td>
+              <td className="border border-gray-200 py-4 whitespace-nowrap text-sm text-gray-500">
+                <input
+                  type="text"
+                  className="outline-none  px-2"
+                  placeholder="Item"
+                />
+              </td>
+              <td className=" border border-gray-200 py-4 whitespace-nowrap text-sm text-gray-900">
+                {" "}
+                <input
+                  type="text"
+                  className=" outline-none  px-2"
+                  placeholder="description"
+                />
+              </td>
+              <td className=" border border-gray-200 w-12 py-4 whitespace-nowrap text-sm text-gray-500">
+                {" "}
+                <input
+                  type="text"
+                  className=" outline-none w-full px-2"
+                  placeholder="Qty"
+                />
+              </td>
+              <td className=" border border-gray-200 w-32 py-4 whitespace-nowrap text-sm text-gray-500">
+                {" "}
+                <input
+                  type=" text"
+                  className="w-full outline-none px-2"
+                  placeholder="Unit Price"
+                />
+              </td>
+              <td className=" border border-gray-200 w-24 py-4 whitespace-nowrap text-sm text-gray-500">
+                {" "}
+                <input
+                  type="text"
+                  className="w-full outline-none px-2"
+                  placeholder="Disc%"
+                />
+              </td>
+              <td className=" border border-gray-200 w-32 py-4 whitespace-nowrap text-sm text-gray-500">
+                {" "}
+                <input
+                  type="text"
+                  className="w-full outline-none px-2"
+                  placeholder="Account"
+                />
+              </td>
+              <td className=" border border-gray-200 py-4 whitespace-nowrap text-sm text-gray-500">
+                {" "}
+                <input
+                  type="text"
+                  className="w-full outline-none px-2"
+                  placeholder="Tax Rate"
+                />
+              </td>
+              <td className=" border border-gray-200 py-4 whitespace-nowrap text-sm text-gray-500">
+                {" "}
+                <input
+                  type="text"
+                  className="w-full outline-none px-2"
+                  placeholder="Amount"
+                />
+              </td>
+              <td className=" border border-gray-200 py-4 whitespace-nowrap text-sm text-gray-500">
+                {" "}
+                x
+              </td>
+            </tr>
+          ))}
+
           {/* Add more rows as needed */}
         </tbody>
       </table>
